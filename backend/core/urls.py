a839a2374ce4d3b5cdb5c03c9709b366c66088c6
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-02-14 07:20:34 +0000

    MED-12 Added User Auth

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import UserView
+from .views import LoginView, RegisterView, UserView
 
 urlpatterns = [
-    path("register", UserView.as_view()),
+    path("register", RegisterView.as_view()),
+    path("login", LoginView.as_view()),
+    path("user", UserView.as_view()),
 ]
