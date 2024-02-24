049a69712373a300ae997c5f76f703d1f868617d
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-02-24 11:10:49 +0000

    added poetry support

--- run.bash
+++ run.bash
@@ -1,8 +1,4 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt --quiet
-
-
+poetry install && poetry shell
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
