00501305109620a786ff50f46411e194e32e277f
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-02-27 16:43:16 +0000

    Backend testing complete

--- backend/tests/test_authentication.py
+++ backend/tests/test_authentication.py
@@ -1,25 +1,57 @@
+from core.models import User
 from django.urls import reverse
 from rest_framework import status
-
-# Other imports
-# from backend.core.models import User
-from backend.apps.core.models import User
 
 from .base import BaseApiTest
 
 
-class AccountTests(BaseApiTest):
-    def test_create_account(self):
-        url = reverse("register")
-        data = {
-            "first_name": "Faisal",
-            "last_name": "Fida",
-            "username": "faisal",
-            "email": "faisal@gmail.com",
-            "password": "ubiquitous",
-            "password_confirm": "ubiquitous",
-        }
+class SignInEndpointTests(BaseApiTest):
+    def setUp(self):
+        super().setUp()
+        user = User.objects.create(email="abc@gmail.com")
+        user.set_password("user@123")
+        user.save()
+
+    def test_with_data(self):
+        url = reverse("login")
+        data = {"email": "abc@gmail.com", "password": "user@123"}
         response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().email, "faisal@gmail.com")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_without_data(self):
+        url = reverse("login")
+        response = self.client.post(url, {}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_invalid_email(self):
+        url = reverse("login")
+        data = {"email": "useremail.com", "password": "user@123"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data, {"error", "Please provide a valid email address"}
+        )
+
+    def test_password_validity(self):
+        url = reverse("login")
+        data = {"email": "abc@gmail.com", "password": "user123"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            response.data,
+            {
+                "error": "Sorry, we could not find a user with the provided credentials. Please try again.",
+            },
+        )
+
+    def test_user_exist(self):
+        url = reverse("login")
+        data = {"email": "xyz@gmail.com", "password": "user@123"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            response.data,
+            {
+                "error": "Sorry, we could not find a user with the provided credentials. Please try again."
+            },
+        )
