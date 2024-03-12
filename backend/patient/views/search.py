d4aaf20d67611d2acd4394ed2646b23c7d3c2612
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-03-12 11:05:46 +0000

    search and filter

--- backend/patient/views/search.py
+++ backend/patient/views/search.py
@@ -21,4 +21,10 @@ class SearchDoctorView(generics.ListAPIView):
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
-        return Response({"results": serializer.data, "count": len(serializer.data)})
+        return Response(
+            {
+                "results": serializer.data,
+                "status": 200,
+                "Data Length: ": len(serializer.data),
+            }
+        )
