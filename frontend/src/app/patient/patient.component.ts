5c270520f91ab18c7d9d8ff6db80b15fe6238471
Atiq Ur Rehman <142742812+AtiqBytes@users.noreply.github.com>
2024-03-16 06:44:14 +0000

    intial 404

--- frontend/src/app/patient/patient.component.ts
+++ frontend/src/app/patient/patient.component.ts
@@ -5,56 +5,53 @@ import { PatientService } from '../services/patient/patient.service';
 @Component({
   selector: 'app-patient',
   templateUrl: './patient.component.html',
-  styleUrls: ['./patient.component.css'],
+  styleUrls: ['./patient.component.css']
 })
 export class PatientComponent {
   form!: FormGroup;
-  searchTimeout: any = null;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private patientService: PatientService
-  ) {}
+  constructor(private formBuilder: FormBuilder,private patientService:PatientService) {}
 
-  formSubmitted: boolean = false;
-  searchQuery: string = '';
-  searchResults: any[] = [];
-
+  formSubmitted:boolean=false;
+  
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      age: '',
-      sex: '',
-      blood_group: '',
-      weight: '',
-      height: '',
-      bmi: '',
-    });
+  
+  this.form = this.formBuilder.group( {
+  
+    age:'',
+
+    sex:'',
+
+    blood_group:'',
+
+    weight:'',
+
+    height:'',
+
+    bmi:''
+  
+  
+  });
+  
   }
+
 
   submit() {
-    this.patientService
-      .register(this.form.getRawValue())
-      .subscribe((res) => console.log(res));
-    this.formSubmitted = true;
+    this.patientService.register(this.form.getRawValue()).subscribe(
+      (res)=>console.log(res)
+    )
+    this.formSubmitted=true;
   }
 
-  getProfilePhotoUrl(url: string): string {
-    const currentHost = window.location.origin.replace('4200', '8000');
-    return url.replace('http://localhost:8000', currentHost);
-  }
+  searchQuery:string='';
+  searchResults:any[]=[];
 
-  onSearchQueryChange() {
-    clearTimeout(this.searchTimeout);
-    this.searchTimeout = setTimeout(() => {
-      this.searchDoctors();
-    }, 500);
-  }
-
-  searchDoctors() {
-    this.patientService
-      .searchDoctors(this.searchQuery)
-      .subscribe((response: any) => {
-        this.searchResults = response;
-      });
-  }
+  searchDoctors()
+  {
+    this.patientService.searchDoctors(this.searchQuery).subscribe(
+      (response:any)=>{
+          this.searchResults=response.results
+      }
+    )
+    }
 }
