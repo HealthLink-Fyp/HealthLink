b0d12d09da2944cb383d829e7761712e8d0b495d
Katara <63296827+dare-me-devil@users.noreply.github.com>
2024-03-16 20:43:49 +0000

    settings.py update

--- backend/healthlink/settings/base.py
+++ backend/healthlink/settings/base.py
@@ -29,16 +29,11 @@ DEBUG = False
 USE_TZ = True
 TIME_ZONE = "UTC"
 
-# CORS Settings
 # CORS settings
-# CORS_ALLOWED_ORIGINS = [FRONTEND_URL]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-# CSRF_TRUSTED_ORIGINS = [FRONTEND_URL]
-
 # Quick and dirty way to enable all hosts for development
-# ALLOWED_HOSTS = [BACKEND_URL, FRONTEND_URL]
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
