59bf62d71328fe8e65828195c39dd4b09681b0fe
Katara <63296827+dare-me-devil@users.noreply.github.com>
2024-03-17 07:20:35 +0000

    env

--- backend/patient/urls.py
+++ backend/patient/urls.py
@@ -3,10 +3,6 @@ from django.urls import path
 from patient.views import search
 
 urlpatterns = [
-    path("search/doctors/", search.SearchDoctorView.as_view(), name="search"),
-    path(
-        "search/doctors/autocomplete/",
-        search.AutoCompleteDoctorView.as_view(),
-        name="autocomplete",
-    ),
+    path("search/doctors/", search.SearchDoctorView.as_view()),
+    path("search/doctors/autocomplete/", search.AutoCompleteDoctorView.as_view()),
 ]
