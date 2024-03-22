0997945570417f66ce7cea76f47260ef357a6a46
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-03-22 12:50:43 +0000

    Search testing complete

--- backend/tests/test_search.py
+++ backend/tests/test_search.py
@@ -13,15 +13,16 @@ class SearchTests(DoctorApiTest):
         self.serializer_data = DoctorAutoCompleteSerializer(instance=self.doctor).data
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data[0], "\n\n", self.serializer_data)
-        # self.assertEqual(response.data, self.serializer_data)
+        response.data = dict(response.data[0])
+        self.assertDictEqual(response.data, self.serializer_data)
 
-    # def test_autocomplete_doctor(self):
-    #     self.url = reverse("autocomplete") + "?city=Demo"
-    #     self.serializer_data = DoctorAutoCompleteSerializer(instance=self.doctor).data
-    #     response = self.client.get(self.url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data, self.serializer_data)
+    def test_search_doctor(self):
+        self.url = reverse("search") + "?city=Demo"
+        self.serializer_data = DoctorProfileSerializer(instance=self.doctor).data
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response.data = dict(dict(response.data)["results"][0])
+        self.assertDictContainsSubset(response.data, self.serializer_data)
 
     def test_doctor_detail(self):
         self.url = reverse("doctor-detail", kwargs={"pk": self.doctor.pk})
