4ab393f4dc5ee75b90b78794dabb87227c198f2f
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-04-17 14:36:53 +0000

    django settings

--- backend/patient/models.py
+++ backend/patient/models.py
@@ -70,3 +70,23 @@ class MedicineShop(models.Model):
 
     def __str__(self):
         return self.name
+
+
+##------------- Medical Test Model --------------##
+
+
+class MedicalTest(models.Model):
+    """
+    Model to store the medical test details.
+    """
+
+    test_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    link = models.URLField()
+    lab_name = models.CharField(max_length=100)
+    image = models.URLField()
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.name
