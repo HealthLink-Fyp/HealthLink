102ffa64f5fe38d24833251b8963321d49766246
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-05-02 17:18:27 +0000

    backend working

--- backend/healthlink/asgi.py
+++ backend/healthlink/asgi.py
@@ -1,11 +1,14 @@
+import django
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from chat.middleware import JwtAuthMiddleware
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthlink.settings.local")
+django.setup()
+
+from django.core.asgi import get_asgi_application  # noqa: E402
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from channels.security.websocket import AllowedHostsOriginValidator  # noqa: E402
+from chat.middleware import JwtAuthMiddleware  # noqa: E402
+from chat.routing import websocket_urlpatterns  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
