c4847ae4119aca50f9e40d460f1cd7a5d1ce7259
Katara <63296827+dare-me-devil@users.noreply.github.com>
2024-05-21 13:14:55 +0000

    check

--- backend/patient/serializers.py
+++ backend/patient/serializers.py
@@ -3,13 +3,15 @@ from .models import Appointment, MedicalRecord, MedicineShop, MedicalTest
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source="doctor.full_name", read_only=True)
+
     class Meta:
         model = Appointment
         fields = "__all__"
 
 
 class MedicalRecordSerializer(serializers.ModelSerializer):
-    doctor_name = serializers.CharField(source="doctor.full_name", read_only=True)
+    full_name = serializers.CharField(source="doctor.full_name", read_only=True)
 
     class Meta:
         model = MedicalRecord
