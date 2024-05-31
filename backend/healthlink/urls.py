afa39c1ddeb8a2d9e9c2104877920f63ae818f55
Faisal Fida <69955157+faisal-fida@users.noreply.github.com>
2024-05-31 11:49:56 +0000

    index.html for api

--- backend/healthlink/urls.py
+++ backend/healthlink/urls.py
@@ -4,8 +4,29 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+# index.html
+from django.views.generic import TemplateView
+
+from django.contrib.sites.models import Site
+
+site = Site.objects.get_current()
+current_site = site.domain
+
+
+api_info = {
+    "Status": "success",
+    "Message": "Welcome to HealthLink API",
+    "Version": "1.0.0",
+}
 
 urlpatterns = [
+    path(
+        "",
+        TemplateView.as_view(
+            template_name="index.html", extra_context={"api_info": api_info}
+        ),
+        name="index",
+    ),
     path("admin/", admin.site.urls),
     path("api/v1/", include("core.urls")),
     path("api/v1/", include("patient.urls")),
--- backend/scripts/setup.sh
+++ backend/scripts/setup.sh
@@ -2,35 +2,16 @@
 
 # Update package list
 sudo apt-get update
-
+sudo apt-get clean
+sudo apt-get autoremove
 # Install pip
 sudo apt install python3-pip
-
-# Install Poetry
-pip install poetry
-
-# Add Poetry to PATH
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-
-# Install Just
-wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
-echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
-sudo apt update
-sudo apt install just
-
-# Install opencv
-sudo apt-get install python3-opencv
-
-# Create a new virtual environment
-cd backend
-poetry shell
-poetry install
-
-# Run database migrations
-just db
-
-# Create a superuser
-just admin
-
-# Start the development server
-just
+pip install --upgrade pip
+# Install dependencies
+pip install --no-cache-dir -r requirements.txt
+python3 -m spacy download en_core_web_lg
+pip install gunicorn
+# Django setup
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
