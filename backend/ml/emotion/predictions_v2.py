0c8fd76f73b84ecd37e2ba5df0c1c1dc1051b2e7
Katara <63296827+dare-me-devil@users.noreply.github.com>
2024-06-09 13:18:15 +0000

    req

--- backend/ml/emotion/predictions_v2.py
+++ backend/ml/emotion/predictions_v2.py
@@ -16,8 +16,8 @@ class EmotionPredictor:
         "surprise",
         "neutral",
     )
-    model_path: str = "ml/emotion/model.tflite"
-    face_path: str = "ml/emotion/face.xml"
+    model_path: str = os.path.join(os.path.dirname(__file__), "emotion_model.tflite")
+    face_path: str = os.path.join(os.path.dirname(__file__), "face.xml")
 
     def __init__(self):
         self._model = None
